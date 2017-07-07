a4f99fdca36077b63fd9c9a6b0977f189db144ec
jdcortes@fullsail.edu <jdcortes@fullsail.edu>
2017-07-07 16:10:27 -0400

    LinQ stuff

--- C#/Shapes/Shapes/Program.cs
+++ C#/Shapes/Shapes/Program.cs
@@ -10,16 +10,41 @@ namespace Shapes
     {
         static void Main(string[] args)
         {
-            theShapes shape = new theShapes();
-            Rectangle rect = new Rectangle();
-            Triangle tri = new Triangle();
-            Square sqr = new Square();
+            
 
-            Console.WriteLine(shape.GetArea());
-            Console.WriteLine(rect.GetArea());
-            Console.WriteLine(tri.GetArea());
-            Console.WriteLine(sqr.GetArea());
+            List<theShapes> shapes = new List<theShapes>();
+            shapes.Add(new Square(5));
+            shapes.Add(new Square(1));
+            shapes.Add(new Triangle(2, 5));
+            shapes.Add(new Triangle(1, 1));
+            shapes.Add(new Rectangle(4, 6));
+            shapes.Add(new Rectangle(3, 9));
+            shapes.Add(new theShapes(6, 7));
 
+            Dictionary<string, theShapes> dict = new Dictionary<string, theShapes>();
+            foreach (var item in dict.Keys)
+            {
+                Console.WriteLine(item + ", ");
+            }
+            Console.WriteLine("\n\nprint shapes with area equal or greater than 4: ");
+            var res = shapes.Where(o => o.Area() >= 4).ToList();
+            var rectlist = shapes.Where(r => r is Rectangle).ToList();
+
+            foreach (var item in rectlist)
+            {
+                Console.WriteLine(item.GetType());
+                Console.WriteLine(item.ToString());
+            }
+
+            var triList = (from t in shapes where t is Triangle select t).ToList();
+            foreach (var item in res)
+            {
+                Console.WriteLine(item.GetType());
+                Console.WriteLine(item.Area());
+                Console.WriteLine(item.ToString());
+            }
+            var res2 = shapes.Where(o => o.Area() < 4).ToList();
+            Console.WriteLine($"\n shapes w/ area < 4, count is {res2.Count}");
             Console.ReadLine();
         }
     }
--- C#/Shapes/Shapes/theShapes.cs
+++ C#/Shapes/Shapes/theShapes.cs
@@ -8,29 +8,77 @@ namespace Shapes
 {
     public class theShapes
     {
-        double lenght = 5;
-        double width = 5;
-
-        public double GetArea()
+        public theShapes()
         {
-            return this.lenght * this.width;
+            height = 0;
+            width = 0;
         }
 
+        public theShapes(double l, double w)
+        {
+            height = l;
+            width = w;
+        }
+
+        double height = 5;
+        double width = 5;
+        public virtual double Area()
+        {
+            return this.height * this.width;
+        }
+
+        public string GetShapeInfo()
+        {
+            string result = "";
+
+            result = $"lenght: {height} \n and width {width}";
+
+            return result;
+        }
+        public override string ToString()
+        {
+            return $"The shape: {GetShapeInfo()}";
+        }
     }
     class Rectangle:theShapes
     {
+        public Rectangle(double heigth, double width) : base(heigth, width)
+        {
 
+        }
     }
     class Square: Rectangle
     {
-                       
+        public Square(int length) : base(length, length)
+        {
+
+        }
     }
     class Triangle: theShapes
     {
-        
-        public double GetArea()
+        public Triangle(int heigth, int width) : base(heigth, width)
         {
-            return base.GetArea() / 2;
+        }
+
+        /// <summary>
+        /// override the base Shape Area() method
+        /// </summary>
+        /// <returns></returns>
+        public override double Area()
+        {
+            return base.Area() / 2;
+        }
+    }
+    class Circle : theShapes
+    {
+        public Circle(int heigth, int width) : base(heigth, width)
+        {
+        }
+    }
+    class ShapeZeroAreaException : Exception
+    {
+        public ShapeZeroAreaException(string message) : base(message)
+        {
         }
     }
 }
