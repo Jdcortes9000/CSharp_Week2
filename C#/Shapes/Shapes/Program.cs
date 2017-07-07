2cc51052b99f42f5e0df4d39e39ff17892c3d5f1
jdcortes@fullsail.edu <jdcortes@fullsail.edu>
2017-07-07 13:50:46 -0400

    Repository made

---
+++ C#/Shapes/Shapes/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shapes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            theShapes shape = new theShapes();
+            Rectangle rect = new Rectangle();
+            Triangle tri = new Triangle();
+            Square sqr = new Square();
+
+            Console.WriteLine(shape.GetArea());
+            Console.WriteLine(rect.GetArea());
+            Console.WriteLine(tri.GetArea());
+            Console.WriteLine(sqr.GetArea());
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ C#/Shapes/Shapes/theShapes.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shapes
+{
+    public class theShapes
+    {
+        double lenght = 5;
+        double width = 5;
+
+        public double GetArea()
+        {
+            return this.lenght * this.width;
+        }
+
+    }
+    class Rectangle:theShapes
+    {
+
+    }
+    class Square: Rectangle
+    {
+                       
+    }
+    class Triangle: theShapes
+    {
+        
+        public double GetArea()
+        {
+            return base.GetArea() / 2;
+        }
+    }
+}
